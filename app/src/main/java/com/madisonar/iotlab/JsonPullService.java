48ce0875979970899255f737c2f4a32533970acf
Nicholas Sielicki <nlsielicki@wisc.edu>
2014-11-22 23:42:11 -0600

    * Finished up writing JsonPullService and NearbyBuilder. Testing needs to be done.

--- app/src/main/java/com/madisonar/iotlab/JsonPullService.java
+++ app/src/main/java/com/madisonar/iotlab/JsonPullService.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,22 +27,21 @@ import java.util.List;
  *     the MadisonAR server and passes the JSON back to the caller.
  * </p>
  */
-public class JsonPullService extends IntentService{
-
+public class JsonPullService extends IntentService
+{
+    public static final String PARAM_OUT_MSG = "out";
     /** Constructor. Simply calls superclass constructor with class name.
      */
     public JsonPullService()
     {
         super("JsonPullService");
     }
-
     /** The handler when this class is invoked.
-     * @param general android Intent.
+     * @param inputIntent android Intent.
      */
     @Override
-    protected void onHandleIntent(Intent intentP) {
-
-        // make a location manager from the context we inherit from intentP.
+    protected void onHandleIntent(Intent inputIntent) {
+        // make a location manager from the context we inherit from inputIntent.
         LocationManager lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
         // grab the current location from LM
         Location currLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -53,6 +53,8 @@ public class JsonPullService extends IntentService{
         HttpPost httpP = new HttpPost(url);
         try
         {
+
+
             // To hold our paramters.
             List<NameValuePair> params = new ArrayList<NameValuePair>(2);
             // MadisonAR server expects two key value pairs, lat and lng to determine location and
@@ -61,11 +63,23 @@ public class JsonPullService extends IntentService{
             params.add(new BasicNameValuePair("lng", String.valueOf(currLocation.getLongitude())));
             // Update our HTTPPost with the new parameters
             httpP.setEntity(new UrlEncodedFormEntity(params));
-            // Execute the query into a new HttpResponse.
-            HttpResponse response = httpC.execute(httpP);
-            Intent responseIntent = new Intent();
-            responseIntent.setAction(NearbyBuilder.ResponseReceiver)
-            response.toString();
+            // Execute the command and get the entity representing it so we can get the content.
+            HttpEntity entity = httpC.execute(httpP).getEntity();
+            String rawJSON = EntityUtils.toString(entity);
+
+
+            // Create a new intent for the response.
+            Intent outputIntent = new Intent();
+            // Set the action to our handlers ACTION_RESP
+            outputIntent.setAction(NearbyBuilder.ResponseReceiver.ACTION_RESP);
+            // Add a category. TODO: Decide if this is needed. Removed for now.
+            // outputIntent.addCategory(Intent.CATEGORY_DEFAULT);
+
+            // Put our message in as an extra with the flag "out" so that we can grab it in the
+            // caller.
+            outputIntent.putExtra(PARAM_OUT_MSG, rawJSON);
+            // broadcast the intent so that our caller can handle it.
+            sendBroadcast(outputIntent);
         }
         catch (Exception e)
         {
--- app/src/main/java/com/madisonar/iotlab/NearbyBuilder.java
+++ app/src/main/java/com/madisonar/iotlab/NearbyBuilder.java
@@ -6,29 +6,93 @@ import android.content.Intent;
 
 import com.madisonar.iotlab.model.Building;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
-public class NearbyBuilder {
+public class NearbyBuilder
+{
+    //private static final String TAG = NearbyBuilder.class.getSimpleName();
 
-    private static final String TAG = NearbyBuilder.class.getSimpleName();
+    private ArrayList<Building> mBuildings;
+    private double unixTime = 0;
 
-    private final ArrayList<Building> mBuildings;
+
+    private double requestLat;
+    private double requestLng;
+    private int status = 1;
 
     public class ResponseReceiver extends BroadcastReceiver{
         public static final String ACTION_RESP =
                 "com.madisonar.iotlab.intent.action.MESSAGE_PROCESSED";
 
         @Override
-        public void onReceive(Context c, Intent i)
+        public void onReceive(Context receivedContext, Intent receivedIntent)
         {
+            String rawJSON = receivedIntent.getStringExtra(JsonPullService.PARAM_OUT_MSG);
+            try
+            {
+                ArrayList<Building> newBuildings = new ArrayList<Building>();
+                JSONObject container = new JSONObject(rawJSON);
 
+                JSONObject response = container.getJSONObject("response");
+                double newTime = response.getDouble("unixTime");
+                double newRequestLat = response.getDouble("requestLat");
+                double newRequestLng = response.getDouble("requestLng");
+                JSONArray buildingJSONArray = response.getJSONArray("buildings");
+                for (int i = 0; i < buildingJSONArray.length(); i++)
+                {
+                    JSONObject currentBuilding = buildingJSONArray.getJSONObject(i);
+                    Building toAdd = new Building(
+                            currentBuilding.getDouble("lLat"),
+                            currentBuilding.getDouble("lLng"),
+                            currentBuilding.getDouble("rLat"),
+                            currentBuilding.getDouble("rLng"),
+                            currentBuilding.getDouble("rHeading"),
+                            currentBuilding.getDouble("lHeading"),
+                            currentBuilding.getString("bName"));
+                    newBuildings.add(toAdd);
+                }
+                mBuildings = newBuildings;
+                unixTime = newTime;
+                requestLat = newRequestLat;
+                requestLng = newRequestLng;
+            }
+            catch (JSONException e)
+            {
+                status = 1;
+            }
         }
     }
 
     public NearbyBuilder(Context context)
     {
         mBuildings = new ArrayList<Building>();
-        Intent getJSON = new Intent(context, JsonPullService.class);
-        context.startService(getJSON);
+        Intent inputIntent = new Intent(context, JsonPullService.class);
+        context.startService(inputIntent);
+    }
+
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public double getUnixTime() {
+        return unixTime;
+    }
+
+    public double getRequestLat() {
+        return requestLat;
+    }
+
+    public double getRequestLng() {
+        return requestLng;
+    }
+
+    public ArrayList<Building> getmBuildings() {
+        return mBuildings;
     }
 }
